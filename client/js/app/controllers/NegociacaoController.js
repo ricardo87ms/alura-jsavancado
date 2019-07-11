cd48e37cac962125b0d2a882194d05d4e89813ba
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-07-11 18:09:28 -0300

    Corrigindo o problema com a data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,9 +12,13 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
 
-        let date = new Date(this._inputData.value.split('-'));
+        let data = new Date(...this._inputData.value
+                                              .split('-')
+                                              .map((item, indice) => item - indice % 2)
+        );
+
         let negociacao = new Negociacao(
-            date,
+            data,
             this._inputQuantidade.value,
             this._inputValor.value
         );
