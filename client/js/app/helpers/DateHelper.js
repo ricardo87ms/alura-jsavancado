e9963231dacb27eb81035416cf7e58b38f43b40c
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-07-15 17:39:30 -0300

    Classes estáticas

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,11 +1,15 @@
 class DateHelper {
 
-    textoParaData(texto) {
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada!');
+    }
+
+    static textoParaData(texto) {
         return new Date(...texto.split('-')
                                 .map((item, indice) => item -  indice % 2));
     }
 
-    dataParaTexto(data) {
+    static dataParaTexto(data) {
         return data.getDate()
                     + '/' + (data.getMonth() + 1)
                     + '/' + data.getFullYear()                   
