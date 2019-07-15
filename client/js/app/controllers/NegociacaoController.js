65fcb0b5b38cdfd56ae6c86e8065607603365ef3
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-07-15 17:33:54 -0300

    Criando o helper de data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,22 +12,15 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value
-                                              .split('-')
-                                              .map((item, indice) => item -  indice % 2)
-        );
+        let helper = new DateHelper();
 
         let negociacao = new Negociacao(
-            data,
+            helper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+        console.log(negociacao);
 
-        let dataParaTexto = negociacao.data.getDate()
-                            + '/' + (negociacao.data.getMonth() + 1)
-                            + '/' + negociacao.data.getFullYear()                   
-        ;
-
-        console.log(dataParaTexto);
+        console.log(helper.dataParaTexto(negociacao.data));
     }
 }
