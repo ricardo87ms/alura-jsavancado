d12e9061453d69f19415b92e747eec2abd520d1a
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-08-23 18:58:44 -0300

    Monkey patch

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,9 +1,11 @@
 var ConnectionFactory = (function () {
-    var stores = ['negociacoes'];
-    var version = 3;
-    var dbName = 'aluraframe';
+    const stores = ['negociacoes'];
+    const version = 3;
+    const dbName = 'aluraframe';
 
     var connection = null;
+
+    var close = null;
 
     return class ConnectionFactory {
 
@@ -25,7 +27,13 @@ var ConnectionFactory = (function () {
 
                 openRequest.onsuccess = e => {
 
-                    if (!connection) connection = e.target.result;
+                    if (!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function () {
+                            throw new Error('Você não pode fechar diretamente a conexão');
+                        };
+                    }
                     resolve(connection);
                 };
 
@@ -44,6 +52,15 @@ var ConnectionFactory = (function () {
                 if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
                 connection.createObjectStore(store, { autoIncrement: true });
             });
+        }
+
+        static closeConnection() {
+
+            if (connection) {
+                close();
+                connection = null;
+
+            }
         }
     }
 })();
