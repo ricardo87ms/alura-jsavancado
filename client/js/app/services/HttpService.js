676968d55d1ac6cb730a0ddf396bf9c446d70574
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-08-24 11:21:01 -0300

    Ajustando o método get para utilizar o fletch api

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,24 +1,33 @@
 class HttpService {
 
+    _handleErrors(res) {
+        if (!res.ok) throw new Error(res.statusText);
+        return res;
+    }
+
     get(url) {
 
-        return new Promise((resolve, reject) => {
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
 
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            }
+        // return new Promise((resolve, reject) => {
 
-            xhr.send();
-        });
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open('GET', url);
+        //     xhr.onreadystatechange = () => {
+        //         if (xhr.readyState == 4) {
+        //             if (xhr.status == 200) {
+        //                 resolve(JSON.parse(xhr.responseText));
+        //             } else {
+        //                 console.log(xhr.responseText);
+        //                 reject(xhr.responseText);
+        //             }
+        //         }
+        //     }
+
+        //     xhr.send();
+        // });
     }
 
     post(url, dado) {
