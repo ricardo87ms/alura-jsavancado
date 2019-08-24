e6ea33462faad8698ec6fd0dda9a2517a447bf02
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-08-24 11:28:43 -0300

    Ajustando o método post para utilizar o flech api

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -32,27 +32,33 @@ class HttpService {
 
     post(url, dado) {
 
+        return fetch(url, {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+            .then(res => this._handleErrors(res));
 
-        return new Promise((resolve, reject) => {
+        // return new Promise((resolve, reject) => {
 
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
+        //     let xhr = new XMLHttpRequest();
+        //     xhr.open("POST", url, true);
+        //     xhr.setRequestHeader("Content-type", "application/json");
+        //     xhr.onreadystatechange = () => {
 
-                if (xhr.readyState == 4) {
+        //         if (xhr.readyState == 4) {
 
-                    if (xhr.status == 200) {
+        //             if (xhr.status == 200) {
 
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
+        //                 resolve(JSON.parse(xhr.responseText));
+        //             } else {
 
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-        });
+        //                 reject(xhr.responseText);
+        //             }
+        //         }
+        //     };
+        //     xhr.send(JSON.stringify(dado));
+        // });
 
     }
 }
