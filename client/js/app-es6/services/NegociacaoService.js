1264d918a26d1bb0462fe6f0452b6531c9b51f29
Ricardo Moreira Soares <ricardo87ms@gmail.com>
2019-08-26 17:21:10 -0300

    Tornando nosso código ainda mais compatível usando Babel

---
+++ client/js/app-es6/services/NegociacaoService.js
@@ -0,0 +1,125 @@
+class NegociacaoService {
+
+    constructor() {
+        this._http = new HttpService();
+    }
+
+    obterNegociacoesDaSemana() {
+
+        return new Promise((resolve, reject) => {
+
+            this._http
+                .get('negociacoes/semana')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível obter as negociações da semana');
+                })
+        });
+    }
+
+    obterNegociacoesDaSemanaAnterior() {
+
+        return new Promise((resolve, reject) => {
+
+            this._http
+                .get('negociacoes/anterior')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível obter as negociações da semana anterior');
+                })
+        });
+
+    }
+
+    obterNegociacoesDaSemanaRetrasada() {
+
+        return new Promise((resolve, reject) => {
+
+            this._http
+                .get('negociacoes/retrasada')
+                .then(negociacoes => {
+                    resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                })
+                .catch(erro => {
+                    console.log(erro);
+                    reject('Não foi possível obter as negociações da semana retrasada');
+                })
+        });
+
+    }
+
+    obterNegociacoes() {
+
+        return Promise.all([
+            this.obterNegociacoesDaSemana(),
+            this.obterNegociacoesDaSemanaAnterior(),
+            this.obterNegociacoesDaSemanaRetrasada()
+        ]).then(periodos => {
+
+            let negociacoes = periodos
+                .reduce((dados, periodo) => dados.concat(periodo), [])
+                .map(dado => new Negociacao(new Date(dado.data), dado.quantidade, dado.valor));
+
+            return negociacoes;
+        }).catch(erro => {
+            throw new Error(erro);
+        });
+    }
+
+    cadastra(negociacao) {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(conexao => new NegociacaoDao(conexao))
+            .then(dao => dao.adiciona(negociacao))
+            .then(() => 'Negociação cadastrada com sucesso')
+            .catch(erro => {
+                throw new Error("Não foi possível adicionar a negociação")
+            });
+    }
+
+    lista() {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível obter as negociações')
+            })
+    }
+
+    apaga() {
+
+        return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(() => 'Negociações apagadas com sucesso')
+            .catch(erro => {
+                console.log(erro);
+                throw new Error('Não foi possível apagar as negociações')
+            });
+    }
+
+    importa(listaAtual) {
+
+        return this.obterNegociacoes()
+            .then(negociacoes =>
+                negociacoes.filter(negociacao =>
+                    !listaAtual.some(negociacaoExistente =>
+                        JSON.stringify(negociacao) == JSON.stringify(negociacaoExistente)))
+            )
+            .catch(erro => {
+                console.log(erro);
+                throw new Error("Não foi possível importar as negociações");
+            });
+    }
+}
--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,64 +1,85 @@
-class HttpService {
+'use strict';
 
-    _handleErrors(res) {
-        if (!res.ok) throw new Error(res.statusText);
-        return res;
-    }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-    get(url) {
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-        return fetch(url)
-            .then(res => this._handleErrors(res))
-            .then(res => res.json());
+var HttpService = function () {
+        function HttpService() {
+                _classCallCheck(this, HttpService);
+        }
 
-        // return new Promise((resolve, reject) => {
+        _createClass(HttpService, [{
+                key: '_handleErrors',
+                value: function _handleErrors(res) {
+                        if (!res.ok) throw new Error(res.statusText);
+                        return res;
+                }
+        }, {
+                key: 'get',
+                value: function get(url) {
+                        var _this = this;
 
-        //     let xhr = new XMLHttpRequest();
-        //     xhr.open('GET', url);
-        //     xhr.onreadystatechange = () => {
-        //         if (xhr.readyState == 4) {
-        //             if (xhr.status == 200) {
-        //                 resolve(JSON.parse(xhr.responseText));
-        //             } else {
-        //                 console.log(xhr.responseText);
-        //                 reject(xhr.responseText);
-        //             }
-        //         }
-        //     }
+                        return fetch(url).then(function (res) {
+                                return _this._handleErrors(res);
+                        }).then(function (res) {
+                                return res.json();
+                        });
 
-        //     xhr.send();
-        // });
-    }
+                        // return new Promise((resolve, reject) => {
 
-    post(url, dado) {
+                        //     let xhr = new XMLHttpRequest();
+                        //     xhr.open('GET', url);
+                        //     xhr.onreadystatechange = () => {
+                        //         if (xhr.readyState == 4) {
+                        //             if (xhr.status == 200) {
+                        //                 resolve(JSON.parse(xhr.responseText));
+                        //             } else {
+                        //                 console.log(xhr.responseText);
+                        //                 reject(xhr.responseText);
+                        //             }
+                        //         }
+                        //     }
 
-        return fetch(url, {
-            headers: { 'Content-Type': 'application/json' },
-            method: 'post',
-            body: JSON.stringify(dado)
-        })
-            .then(res => this._handleErrors(res));
+                        //     xhr.send();
+                        // });
+                }
+        }, {
+                key: 'post',
+                value: function post(url, dado) {
+                        var _this2 = this;
 
-        // return new Promise((resolve, reject) => {
+                        return fetch(url, {
+                                headers: { 'Content-Type': 'application/json' },
+                                method: 'post',
+                                body: JSON.stringify(dado)
+                        }).then(function (res) {
+                                return _this2._handleErrors(res);
+                        });
 
-        //     let xhr = new XMLHttpRequest();
-        //     xhr.open("POST", url, true);
-        //     xhr.setRequestHeader("Content-type", "application/json");
-        //     xhr.onreadystatechange = () => {
+                        // return new Promise((resolve, reject) => {
 
-        //         if (xhr.readyState == 4) {
+                        //     let xhr = new XMLHttpRequest();
+                        //     xhr.open("POST", url, true);
+                        //     xhr.setRequestHeader("Content-type", "application/json");
+                        //     xhr.onreadystatechange = () => {
 
-        //             if (xhr.status == 200) {
+                        //         if (xhr.readyState == 4) {
 
-        //                 resolve(JSON.parse(xhr.responseText));
-        //             } else {
+                        //             if (xhr.status == 200) {
 
-        //                 reject(xhr.responseText);
-        //             }
-        //         }
-        //     };
-        //     xhr.send(JSON.stringify(dado));
-        // });
+                        //                 resolve(JSON.parse(xhr.responseText));
+                        //             } else {
 
-    }
-}
+                        //                 reject(xhr.responseText);
+                        //             }
+                        //         }
+                        //     };
+                        //     xhr.send(JSON.stringify(dado));
+                        // });
+                }
+        }]);
+
+        return HttpService;
+}();
+//# sourceMappingURL=HttpService.js.map
